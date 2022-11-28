f74901d23b97a8e782fbb3cf13d6141607f8a022
Ditzler01 <71181685+Ditzler01@users.noreply.github.com>
2022-11-29 00:41:17 +0800

    Fix front end values

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,9 +21,9 @@
                         <label for="first_name" class="col mx-5 col-form-label text-lg-start mt-3">{{ __('First Name') }}</label>
                     </div>
                     <div class="col mx-5">
-                        <input id="first_name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
+                        <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" autocomplete="first_name" autofocus>
 
-                        @error('name')
+                        @error('first_name')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
@@ -33,9 +33,9 @@
                         <label for="last_name" class="col mx-5 col-form-label text-lg-start mt-3">{{ __('Last Name') }}</label>
                     </div>
                     <div class="col mx-5">
-                        <input id="last_name" type="text" class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="first_name">
+                        <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" autocomplete="first_name">
 
-                        @error('name')
+                        @error('last_name')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
@@ -45,7 +45,7 @@
                         <label for="email" class="col mx-5 col-form-label text-lg-start mt-3">{{ __('Email') }}</label>
                     </div>
                     <div class="col mx-5">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
 
                         @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -57,7 +57,7 @@
                         <label for="email" class="col mx-5 col-form-label text-lg-start mt-3">{{ __('Password') }}</label>
                     </div>
                     <div class="col mx-5">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="new-password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" autocomplete="new-password">
 
                         @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -69,7 +69,7 @@
                         <label for="password-confirm" class="col mx-5 col-form-label text-lg-start mt-3">{{ __('Confirm Password') }}</label>
                     </div>
                     <div class="col mx-5">
-                        <input id="password-confirm" type="password" class="form-control @error('password') is-invalid @enderror" name="password_confirmation" required autocomplete="new-password">
+                        <input id="password-confirm" type="password" class="form-control @error('confirm_password') is-invalid @enderror" name="password_confirmation" autocomplete="new-password">
 
                         @error('password')
                             <span class="invalid-feedback" role="alert">
