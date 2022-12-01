7ee3c574fcb8f7544f621a4ec20649c7a372c83d
Ditzler01 <71181685+Ditzler01@users.noreply.github.com>
2022-12-01 13:59:12 +0800

    Re-design Delete and archive note

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,19 @@
         <img src="/assets/svg/add_note.svg" alt="notebook" class="img-fluid" width="500" height="500">
     </div> --}}
 
-    <div class="container-fluid px-md-5 m-0 d-flex justify-content-md-evenly justify-content-center flex-row flex-wrap overflow-auto h-100 gap-3">
-        <div class="card mt-5 note-card" style="width: 20rem; height: 25rem; background-color: rgb(245, 245, 245);">
+    <div class="container-fluid m-0 d-flex justify-content-md-evenly justify-content-center flex-row flex-wrap overflow-auto h-100 gap-3">
+        <div class="card mt-5 note-card" style="min-width: 15rem; max-width: 20rem; height: 25rem; background-color: rgb(245, 245, 245);">
             <div class="card-body overflow-hidden mb-3">
                 <div class="container overflow-hidden">
                     <div class="container-fluid d-flex align-items-center flex-row justify-content-between p-0 mb-3">
-                        <h5 class="card-title text-center m-0">Card title</h5>
-                        <div class="col-auto d-flex flex-row gap-1">
-                            <div class="col-auto p-1 note-button-hover">
-                                <img src="/assets/icon/archive.png" alt="notebook" class="img-fluid" width="20" height="20">
-                            </div>
-                            <div class="col-auto p-1 note-button-hover">
-                                <img src="/assets/icon/trash.png" alt="notebook" class="img-fluid" width="20" height="20">
-                            </div>
-                        </div>
+                        <h5 class="card-title m-0">Card title</h5>
+                        <button id="more-dropdown" type="button" class="btn btn-outline-secondary border-0" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="/assets/icon/more.png" width="16" height="16" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="more-dropdown">
+                            <li><a class="dropdown-item" href="#">Archive</a></li>
+                            <li><a class="dropdown-item" href="#">Delete</a></li>
+                          </ul>
                     </div> 
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -37,7 +36,7 @@
 
     <div class="position-absolute bottom-0 me-md-5 mb-md-5 me-3 mb-3" style="right: 0">
         <div class="round p-3 custom-shadow add-note" style="background-color: rgb(250, 250, 250); height: 3.5rem; width: 3.5rem;">
-            <img src="/assets/icon/add.png" alt="notebook" class="img-fluid" height="100%" width="100%">
+            <img src="/assets/icon/add.png" alt="add-note" class="img-fluid" height="100%" width="100%">
         </div>
     </div>
 
