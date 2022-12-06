da1316782176af7b2a89e43029c3089562ed9a71
Ditzler01 <71181685+Ditzler01@users.noreply.github.com>
2022-12-06 12:33:39 +0800

    Fix migrations

--- database/migrations/2022_11_24_113200_create_user_notes_table.php
+++ database/migrations/2022_11_24_113200_create_user_notes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('user_notes', function (Blueprint $table) 
         {
-            $table->string('user_id')->unsigned();
+            $table->string('user_id');
             $table->string('id')->primary();
             $table->foreign('user_id')->references('id')->on('users');
         });
